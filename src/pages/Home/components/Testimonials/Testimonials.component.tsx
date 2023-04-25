71b58cd4ff598c7a4df401e1866e287452f1b799
lasha sumbadze <lash.sumb@gmail.com>
2023-04-25 22:01:09 +0200

    add display flex to container

--- src/pages/Home/components/Testimonials/Testimonials.component.tsx
+++ src/pages/Home/components/Testimonials/Testimonials.component.tsx
@@ -14,6 +14,7 @@ import {Navigation, Pagination, Virtual} from 'swiper';
 import 'swiper/css';
 
 const TestimonialsContainer = styled(Container)`
+	display: flex;
 	width: 100%;
 	background-color: ${Colors.purpleLighter};
 	flex-direction: column;
